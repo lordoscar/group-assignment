b670fffc273493fe8f143d8d1eadd08b982de22b
LaskaMarcel <laskamarcel96@gmail.com>
2019-05-28 17:32:34 +0200

    Lagt in API (OpenWeather). Kan hämta JSON & parsa den. Visar temperaturen osv för vald stad.
    Ser dock shit ut så får fixa design:)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
+const API_KEY = "c9f2fcc97c2431dabd58531b5f586b59";
 
 
 class App extends Component {
-  render() {
-    return (
-      null
-    );
+  state = {
+  temperature: undefined,
+  city: undefined,
+  country: undefined,
+  humidity: undefined,
+  description: undefined,
+  error: undefined
+}
+getWeather = async (e) => {
+  e.preventDefault();
+  const city = e.target.elements.city.value;
+  const country = e.target.elements.country.value;
+  const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+  const data = await api_call.json();
+  if (city && country) {
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
+  } else {
+    this.setState({
+      temperature: undefined,
+      city: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: "Please enter the values."
+    });
   }
 }
-
+render() {
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-5 title-container">
+                <Titles />
+              </div>
+              <div className="col-xs-7 form-container">
+                <Form getWeather={this.getWeather} />
+                <Weather
+                  temperature={this.state.temperature}
+                  humidity={this.state.humidity}
+                  city={this.state.city}
+                  description={this.state.description}
+                  error={this.state.error}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const Form = (props) => {
+  return (
+    <form onSubmit = {this.props.loadWeather}>
+  <input type="text" name="city" placeholder="City..."/>
+  <input type="text" name="country" placeholder="Country..."/>
+  <button>Get Weather</button>
+</form>
+);
+}
